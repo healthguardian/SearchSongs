463f6c487d26f20f6d03a0e951be32c5a58e3d4a
Maxence Roy <m@masserm.com>
2018-10-03 11:09:39 -0500

    Complete data models for Album, Artist, Genre and Song

---
+++ SearchSongs/Models/Song.swift
@@ -0,0 +1,17 @@
+//
+//  Song.swift
+//  SearchSongs
+//
+//  Created by Maxence Roy on 10/3/18.
+//  Copyright © 2018 Maxence Roy. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    let id: Int
+    let name: String
+    let censoredName: String
+    let trackTime: Int
+    let isExplicit: Bool
+}
